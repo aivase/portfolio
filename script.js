c1d56dbf54204fbb7bfeb3e32ac0cbade42f4770
aivase <einos@proton.me>
2025-05-06 12:52:26 +0300

    add time display of helsinki

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ darkModeToggle.addEventListener('click', () => {
         localStorage.setItem('darkMode', 'enabled'); 
     }
 });
+function updateClock() {
+    const now = new Date();
+    const helsinkiTime = now.toLocaleTimeString("fi-FI", {
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "Europe/Helsinki"
+    });
+    document.getElementById("clock").textContent = `Helsinki ${helsinkiTime}`;
+  }
+  setInterval(updateClock, 1000);
+  updateClock();
+  
